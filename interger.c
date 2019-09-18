3065f443059e14016d4250ea2e6a76b145ca709d
isaacfandosm <55483538+isaacfandosm@users.noreply.github.com>
2019-09-18 10:45:27 +0200

    comments part 2

--- interger.c
+++ interger.c
@@ -1,17 +1,28 @@
+/*********************************************** *
+* @Objective:Create a program which can search for an inputted number through an array of 20 integers.
+* @Author/s: Guillermo Sabate, Felipe Perez and Isaac Fandos
+* @Creation date: 18/09/2019
+* @Last modification date: 18709/2019
+* ************************************************/
+
 //Libraries
 #include <stdio.h>
 
 //Constants
+#MAXINTEGERS 20
 
-//User data types
+//User datatypes
 
 //Functions
 
 
 //Main code 
 int main() {
+  //Variable declaration
   int a[20];
   int i = 0;
+  
+  //For loop to read through all the characters of the array.
   for(i=0; i<20; i++){
     
   }
@@ -20,3 +31,4 @@ int main() {
 
   return 0;
 }
+//End of code;
