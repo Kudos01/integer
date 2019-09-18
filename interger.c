386e5dc10afd775ff46aba012a812a41cfe680ef
isaacfandosm <55483538+isaacfandosm@users.noreply.github.com>
2019-09-18 10:36:35 +0200

    Comments

--- interger.c
+++ interger.c
@@ -1,11 +1,22 @@
+//Libraries
 #include <stdio.h>
 
+//Constants
+
+//User data types
+
+//Functions
+
+
+//Main code 
 int main() {
   int a[20];
   int i = 0;
   for(i=0; i<20; i++){
     
   }
+  
+  
 
   return 0;
 }
