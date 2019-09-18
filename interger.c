c20c49cdf788f246ffd091d0bb3066590a920893
guillesabate <45517965+guillesabate@users.noreply.github.com>
2019-09-18 10:39:21 +0200

    Guille updated this again because he doesn't remem
    ber* how to code in c so duck it

--- interger.c
+++ interger.c
@@ -3,9 +3,24 @@
 int main() {
   int a[20];
   int i = 0;
+  int input;
+  int found = 0;
   for(i=0; i<20; i++){
-    
+    a[i] = i*i;
   }
-
+  
+  printf("Tell me your deepest secret or a random integer:");
+  scanf("%d", &input):
+  
+  for(i=0; i<20 && !found; i++){
+    if (input == a[i]){
+      found = 1;
+    }
+  }
+  
+  if (found){
+    printf("Shit we found your numero");
+  }
+  
   return 0;
 }
